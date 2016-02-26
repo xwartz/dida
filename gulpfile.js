a4687664bbc3ced382775df380a3d20b2541998f
xwartz <stddup@gmail.com>
2016-02-26 22:01:04 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     shell = require('gulp-shell'),
+    rename = require('gulp-rename'),
     del = require('del'),
     packager = require('electron-packager'),
     builder = require('electron-builder'),
@@ -43,15 +44,15 @@ gulp.task('pkg:clean', function () {
 })
 
 gulp.task('pkg:builder:dida', shell.task([
-  'electron-builder \"pkg/dida-darwin-x64/dida.app\" --platform=osx --out=\"pkg/pkg/osx\" --config=builder.json',
-  'electron-builder \"pkg/dida-win32-x64\" --platform=win --out=\"pkg/pkg/win64\" --config=builder.json',
-  'electron-builder \"pkg/dida-win32-ia32\" --platform=win --out=\"pkg/pkg/win32\" --config=builder.json'
+  'electron-builder \"pkg/dida-darwin-x64/dida.app\" --platform=osx --out=\"pkg/osx\" --config=builder.json',
+  'electron-builder \"pkg/dida-win32-x64\" --platform=win --out=\"pkg/win64\" --config=builder.json',
+  // 'electron-builder \"pkg/dida-win32-ia32\" --platform=win --out=\"pkg/win32\" --config=builder.json'
 ]))
 
 gulp.task('pkg:builder:tick', shell.task([
-  'electron-builder \"pkg/ticktick-darwin-x64/ticktick.app\" --platform=osx --out=\"pkg/pkg/osx\" --config=builder-tick.json',
-  'electron-builder \"pkg/ticktick-win32-x64\" --platform=win --out=\"pkg/pkg/win64\" --config=builder-tick.json',
-  'electron-builder \"pkg/ticktick-win32-ia32\" --platform=win --out=\"pkg/pkg/win32\" --config=builder-tick.json'
+  'electron-builder \"pkg/ticktick-darwin-x64/ticktick.app\" --platform=osx --out=\"pkg/osx\" --config=builder-tick.json',
+  'electron-builder \"pkg/ticktick-win32-x64\" --platform=win --out=\"pkg/win64\" --config=builder-tick.json',
+  // 'electron-builder \"pkg/ticktick-win32-ia32\" --platform=win --out=\"pkg/win32\" --config=builder-tick.json'
 ]))
 
 gulp.task('pkg:dida', shell.task([
